0e87bd67ec1336cce777594d164ae338f2113c0d
Lisa <lisa.djaghri@laplateforme.io>
2025-11-25 13:27:18 +0100

    job01

---
+++ jour03/job01/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<button id="button" onclick="showhide()">Oses cliquer azy</button>
+
+<article id="citation"> “Les logiciels et les mosquées, c'est un peu la même chose - d'abord on les construit,
+ensuite on prie.”</article>
+
+<script src="script.js"></script>
+</body>
+</html>
